f9cfbba6230174bdd0fc8660c7ce1382f8c09fb9
jessicaescobedo <jessicaivetteescobedo@gmail.com>
2018-07-10 18:28:56 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -2,14 +2,14 @@
 def breakfast(food="frosted_flakes")
   puts"In the morning, I eat #{food}."
 end
-breakfast
+breakfast("pancakes")
 
 def lunch(food="grilled_cheese")
   puts"Lunch is the best time for #{food}."
 end
-lunch
+lunch("chicken_wings")
 
 def dinner(food="salmon")
   puts"I like eating fish during dinner like #{food}."
 end
-dinner
+dinner("enchiladas")
